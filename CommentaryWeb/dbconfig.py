0331b8129cb0c19e632e2df13715cd69152b5a31
Konosuba404 <59634252+Konosuba404@users.noreply.github.com>
2021-02-07 11:06:17 +0000

    Update dbconfig.py

--- CommentaryWeb/dbconfig.py
+++ CommentaryWeb/dbconfig.py
@@ -1,5 +1,5 @@
 # 配置flask配置对象中键：SQLALCHEMY_DATABASE_URI
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:944343631Hyb@@8.135.61.184:3306/Commentary'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@/Commentary'
 # 配置flask配置对象中键: SQLALCHEMY_COMMIT_TEARDOWN,设置为True,应用会自动在每次结束后提交数据库中的变动
 SQLALCHEMY_COMMIT_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
